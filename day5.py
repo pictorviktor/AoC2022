c7b1269eec802a47650e61c3e26dce8d6bd56f7a
admin <admin@admins-MacBook-Pro.local>
2022-12-05 18:58:44 +0100

    Add existing file

---
+++ day5.py
@@ -0,0 +1,68 @@
+import re
+f = open('/Users/admin/Documents/CodeProjects/AoC2022/AoC2022/inputDay5.txt', 'r')
+moves = f.readlines()
+counter = 0
+test = [4,1,4]
+
+nr = {
+  1:'one',
+  2:"two", 
+  3:"three",
+  4:'four',
+  5:'five',
+  6:'six',
+  7:'seven',
+  8:'eight',
+  9:'nine',
+}
+stackList = [['m','j','c','b','f','r','l','h']
+,['z','c','d']
+,['h','j','f','c','n','g','w']
+,['p','j','d','m','p','s','b']
+,['n','c','d','r','j']
+,['w','l','d','q','p','j','g','z']
+,['p','z','t','f','r','h']
+,['l','v','m','g']
+,['c','b','g','p','f','q','r','j']]
+
+
+for i in moves:
+    moves[counter] = re.split(r"\n|move | from | to ", i)
+    moves[counter] = moves[counter][1:4]
+    moves
+    counter+=1
+
+moves = [[int(x) for x in i] for i in moves]
+#print(moves)
+#the crate stacks
+# one = ['m','j','c','b','f','r','l','h']
+# two = ['z','c','d']
+# three = ['h','j','f','c','n','g','w']
+# four = ['p','j','d','m','p','s','b']
+# five = ['n','c','d','r','j']
+# six = ['w','l','d','q','p','j','g','z']
+# seven = ['p','z','t','f','r','h']
+# eight = ['l','v','m','g']
+# nine = ['c','b','g','p','f','q','r','j']
+
+
+#def stackChange(move):
+
+    #if move[0] == nr[]
+moveBox = []
+
+for move in moves:
+    for i in range(move[0]):
+        
+        moveBox.append(stackList[move[1]-1][-1])
+        stackList[move[1]-1].pop()
+        stackList[move[2]-1].append(moveBox[0])
+        moveBox = []
+
+
+        
+    
+print(stackList)  
+
+for stack in stackList:
+     print(stack[-1])
